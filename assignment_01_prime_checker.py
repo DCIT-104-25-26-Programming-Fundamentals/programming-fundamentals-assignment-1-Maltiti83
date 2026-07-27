13e39b3fc2fb299b67afc38d7be759bc4cbf4431
Rukaya Abdullah <maltitirukaya2024@gmail.com>
2026-07-27 18:13:00 +0000

    Assignment 1

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -35,3 +35,25 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def is_prime(number):
+    if number < 2:
+        return False
+
+    # check for divisors from 2 up to number - 1
+    for i in range (2, number):
+        if number % i == 0:
+            return False
+
+    # if no divisors were found, the number is prime
+    return True
+
+    #Main program
+    num = int(input("Enter a number: "))
+
+    if is_prime(num):
+        print(f"{num} is a prime number:")
+    else:
+        print (f"{num} is a prime number.")
+
+
+
